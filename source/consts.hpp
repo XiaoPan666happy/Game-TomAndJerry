85f677e33ccc747a2c8b6f6c184d564ebec87fe5
XiaoPan <matthew2023@126.com>
2026-07-15 18:29:26 +0800

    fix:T2语病

--- source/consts.hpp
+++ source/consts.hpp
@@ -21,7 +21,7 @@ constexpr short NUM_OF_MOUSE_HOLE = 10;
 
 constexpr wchar_t* TEXT0 = L"确认请按回车";
 constexpr wchar_t* TEXT1 = L"请确认上面这个框能正常在您的屏幕中显示";
-constexpr wchar_t* TEXT2 = L"游戏目标 上下左右移动鼠标 吃到食物后回到家里";
+constexpr wchar_t* TEXT2 = L"上下左右移动玩家(老鼠) 游戏目标 吃到食物后回到家里";
 constexpr wchar_t* TEXT3 = L"玩家吃到了食物";
 constexpr wchar_t* TEXT4 = L"玩家回到了家里 游戏结束 玩家获胜";
 constexpr wchar_t* TEXT5 = L"猫抓住了玩家 游戏结束 玩家失败";
