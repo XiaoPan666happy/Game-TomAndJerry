85f687a58032c2db0eaa147126dc5332022e7327
XiaoPan <matthew2023@126.com>
2026-07-19 19:44:09 +0800

    fix:通关提示闪退

--- source/main.cpp
+++ source/main.cpp
@@ -148,6 +148,7 @@ int main() {
             WriteConsoleW(hConsole, msg.c_str(), msg.length(), &charsWritten, NULL);
             
             is_game_over = true;
+            break;
         }
 
         // 死
@@ -157,6 +158,7 @@ int main() {
             WriteConsoleW(hConsole, TEXT5, wcslen(TEXT5), &charsWritten, NULL);
             
             is_game_over = true;
+            break;
         }
 
         // 获取寻路
